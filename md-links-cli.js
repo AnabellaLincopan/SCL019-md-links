97d487048138dde60eddc4c156bce9eea53701e5
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2022-04-25 18:27:59 -0400

    se limpia código

--- md-links-cli.js
+++ md-links-cli.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
-// const { options } = require('yuserarg');
-// const { getStatus } = require('./lib/md-functions.js');
-// const { getStatus } = require('./lib/md-functions.js');
 const { mdLinks } = require ('./lib/md-index.js');
-//  const yuserarg = require('yuserarg/yuserarg')
-// const path = process.argv[2];
-// // console.log(path) prueba.md
-// const option = process.argv[3]; 
-// -- validate or --stats
-
-
-// const path = process.argv[2];
-// const option = process.argv[3]; 
 
 const userarg = process.argv;
 const options = {}
@@ -24,18 +12,11 @@ if (userarg.some((x) => x === '--validate')) {
   const path = userarg[0] === 'mdLink' ? userarg[1] : userarg[2];
   
 
-// const option = yuserarg(process.argv.slice(2)).argv;
-// console.log(path)
-
-
 mdLinks(path, options).then((res) => {
     res.forEach(element => {
         const info = `Text: ${element.text}` + '\n' + `Href: ${element.href}` + '\n' + `Path: ${element.file}` + '\n';
         const statusLink = `Total links:  ${element.total}`.cyan + '\n' + `Unique links:  ${element.unique}`.cyan + '\n';
         const validateInfo = info + `Response status: ${element.status} ` + '\n' + `Link status: ${element.statusText}` + '\n';
-        //const totalInfo = statusLink + '\n'+ info + `Response status: ${element.status} ` + '\n' + `Link status: ${element.statusText}` + '\n';
-        // const statusAndValidate = statusLink + `Broken links:  ${element.broken}`.red + '\n' + `Valid links:  ${element.valid}`.green+ '\n';
-        const statusAndValidate = statusLink + '\n' + validateInfo + '\n';
          if (options.validate && !options.showStats) {
              if (element.status === 404) {
                     console.log('\x1B[31m' + validateInfo + '\x1B[31m')
@@ -47,14 +28,8 @@ mdLinks(path, options).then((res) => {
         } else if (!options.validate && options.showStats) {
             console.log(statusLink)
         }
-        //  else if (options.validate && options.showStats) {
-        //     console.log(statusAndValidate)
-        // } 
     });
     console.log('Work done!'.blue)
     console.log('\n')
 })
 .catch((err) => {console.log(err)})
-
-
-// mdLinks(path, option).then((results) => console.table(results));
